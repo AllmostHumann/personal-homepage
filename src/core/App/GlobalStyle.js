83912d36fb9edb9b05081d38353ad18e228eb509
Jakub Gluch <jakub.gluch92@gmail.com>
2023-02-15 21:54:15 +0100

    globalstyle change

--- src/core/App/GlobalStyle.js
+++ src/core/App/GlobalStyle.js
@@ -14,5 +14,8 @@ body {
     font-family: 'Inter', sans-serif;
         word-break: break-word;
         padding: 115px 40px 110px 40px;
+        transition: background 0.3s;
+        background-color: ${({ theme }) => theme.color.whiteLilac};
+        letter-spacing: 0.05em;
 }
 `;
