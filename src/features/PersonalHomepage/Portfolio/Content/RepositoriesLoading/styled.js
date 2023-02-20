d63b6f524c0bdf2422d84f9384ff6dd727a66a64
Jakub Gluch <jakub.gluch92@gmail.com>
2023-02-20 20:55:06 +0100

    spinner style correction

--- src/features/PersonalHomepage/Portfolio/Content/RepositoriesLoading/styled.js
+++ src/features/PersonalHomepage/Portfolio/Content/RepositoriesLoading/styled.js
@@ -25,4 +25,13 @@ border-radius: 50%;
 display: inline-block;
 box-sizing: border-box;
 animation: rotation 0.8s linear infinite;
+
+@keyframes rotation {
+    0% {
+        transform: rotate(0deg);
+    }
+    100% {
+        transform: rotate(360deg);
+    }
+    }
 `;
