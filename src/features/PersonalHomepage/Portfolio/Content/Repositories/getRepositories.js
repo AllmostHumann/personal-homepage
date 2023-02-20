4e273edae157bed26d3c8ae56d53aafb5b49d558
Jakub Gluch <jakub.gluch92@gmail.com>
2023-02-20 20:54:46 +0100

    get repositories moved

--- src/features/PersonalHomepage/Portfolio/Content/Repositories/getRepositories.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const repoURL = "https://api.github.com/users/AllmostHumann/repos";
-
-export const getRepositories = () =>
-    axios
-        .get(repoURL)
-        .then((response) => response.data);
