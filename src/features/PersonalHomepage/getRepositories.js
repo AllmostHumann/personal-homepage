a727cc4fc8e5f6ffed2806fbe2d86ea8d485fbb7
Jakub Gluch <jakub.gluch92@gmail.com>
2023-02-20 20:53:39 +0100

    get repositories created

---
+++ src/features/PersonalHomepage/getRepositories.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+const repoURL = "https://api.github.com/users/AllmostHumann/repos";
+
+export const getRepositories = () =>
+    axios
+        .get(repoURL)
+        .then((response) => response.data);
