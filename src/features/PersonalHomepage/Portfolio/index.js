b1b69bce1c3215434c61639fc3db6247811ee962
Jakub Gluch <jakub.gluch92@gmail.com>
2023-02-20 20:54:37 +0100

    Content component implement

--- src/features/PersonalHomepage/Portfolio/index.js
+++ src/features/PersonalHomepage/Portfolio/index.js
@@ -1,8 +1,19 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { StyledArticle, Icon, Header, Text } from "./styled";
 import { ReactComponent as Github } from "./icons/github.svg";
 import { Content } from "./Content";
+import { fetchRepositories, selectRepositories, selectRepositoriesStatus } from "../personalHomepageSlice";
 
 const Portfolio = () => {
+    const dispatch = useDispatch();
+
+    const repositoriesStatus = useSelector(selectRepositoriesStatus);
+    const repositories = useSelector(selectRepositories);
+
+    useEffect(() => {
+        dispatch(fetchRepositories());
+    }, [dispatch]);
 
     return (
         <>
@@ -12,8 +23,11 @@ const Portfolio = () => {
                 </Icon>
                 <Header>Portfolio</Header>
                 <Text>My recent projects</Text>
-                <Content/>
-            </StyledArticle>
+                </StyledArticle>
+                <Content
+                    status={repositoriesStatus}
+                    repositories={repositories}
+                />
         </>
     );
 };
