6a338f1f67cd90ee2cd3d4b134179b7df6f7f163
Jakub Gluch <jakub.gluch92@gmail.com>
2023-02-20 20:55:39 +0100

    Repositiories component created

--- src/features/PersonalHomepage/Portfolio/Content/Repositories/index.js
+++ src/features/PersonalHomepage/Portfolio/Content/Repositories/index.js
@@ -1,20 +1,22 @@
 import { Wrapper, Container, Title, Description, LinkContainer, Text, Link } from "./styled";
 
 export const Repositories = ({ repositories }) => {
+
   return (
     <Wrapper>
-      {repositories.map(({ id, name, description, html_url, homepage }) => (<Container key={id}>
-        <Title>{name}</Title>
-        <Description>{description}</Description>
-        <LinkContainer>
-          <Text>Demo:</Text>
-          <Link target="_blank" href={homepage}>Visit page</Link>
-        </LinkContainer>
-        <LinkContainer>
-          <Text>Code:</Text>
-          <Link target="_blank" href={html_url}>Browse repository</Link>
-        </LinkContainer>
-      </Container>
+      {repositories.map(({ id, name, description, html_url, homepage }) => (
+        <Container key={id}>
+          <Title>{name}</Title>
+          <Description>{description}</Description>
+          <LinkContainer>
+            <Text>Demo:</Text>
+            <Link target="_blank" href={homepage}>Visit page</Link>
+          </LinkContainer>
+          <LinkContainer>
+            <Text>Code:</Text>
+            <Link target="_blank" href={html_url}>Browse repository</Link>
+          </LinkContainer>
+        </Container>
       ))};
     </Wrapper>
   );
