7bd4e85ff1d1e62c1a6855e6c2ffaf6893d3b07c
Jakub Gluch <jakub.gluch92@gmail.com>
2023-02-22 21:44:31 +0100

    themeSaga added

--- src/core/saga.js
+++ src/core/saga.js
@@ -1,8 +1,10 @@
 import { all } from "redux-saga/effects";
 import { personalHomepageSaga } from "../features/PersonalHomepage/personalHomepageSaga";
+import { themeSaga } from "../common/themeSaga";
 
 export default function* saga() {
     yield all([
         personalHomepageSaga(),
+        themeSaga(),
     ]);
 };
