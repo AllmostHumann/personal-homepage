02dfe24694dcbf8c14a6c8dd0c98aded06747c21
Jakub Gluch <jakub.gluch92@gmail.com>
2023-02-22 21:43:54 +0100

    stylec for media created and code correction

--- src/features/PersonalHomepage/Header/styled.js
+++ src/features/PersonalHomepage/Header/styled.js
@@ -7,6 +7,11 @@ export const Wrapper = styled.div`
     grid-gap: 66px;
     align-items: center;
     margin-bottom: 63px;
+
+    @media(max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
+        grid-template-columns: 1fr;
+        grid-gap: 13px;
+    }
 `;
 
 export const Image = styled.img`
@@ -14,33 +19,44 @@ export const Image = styled.img`
     height: auto;
     border-radius: 50%;
     transition: 0.7s;
+
+    @media(max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
+        width: 133px;
+        height: 133px;
+    }
 `;
 
 export const Content = styled.div``;
 
-export const SubHeader = styled.header`
+export const SubHeader = styled.h2`
+    font-weight: 700;
     font-size: 12px;
     line-height: 1.3;
     margin-bottom: 12px;
     text-transform: uppercase;
-    color: ${({ theme }) => theme.color.mineShaft};
+    color: ${({ theme }) => theme.colors.site.text};
 `;
 
 export const Name = styled.header`
     font-weight: 900;
-    font-size: 38px;
+    font-size: clamp(22px, 5vw, 38px);
     letter-spacing: 0.05em;
+    color: ${({ theme }) => theme.colors.textPrimary};
 `;
 
 export const Info = styled.p`
     font-weight: 400;
-    font-size: 20px;
+    font-size: clamp(17px, 5vw, 20px);
     line-height: 1.4;
     max-width: 633px;
     margin-top: 35px;
     margin-bottom: 32px;
     letter-spacing: 0.05em;
-    color: ${({ theme }) => theme.color.slateGray};
+    color: ${({ theme }) => theme.colors.site.text};
+
+    @media(max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
+        margin-top: 16px;
+    }
 `;
 
 export const Message = styled(MessageItem)`
--- src/features/PersonalHomepage/Tile/styled.js
+++ src/features/PersonalHomepage/Tile/styled.js
@@ -4,8 +4,14 @@ import { ReactComponent as DotItem } from "./icons/dot.svg"
 export const StyledTile = styled.article`
     margin-bottom: 72px;
     padding: 32px 33px 32px 32px;
-    background: ${({ theme }) => theme.color.white};
+    height: fit-content;
+    background: ${({ theme }) => theme.colors.boxBackground};
     box-shadow: 0px -2px 50px rgba(9, 10, 51, 0.02), 0px 16px 58px rgba(9, 10, 51, 0.03);
+
+    @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
+        margin-bottom: 50px;
+        padding: 16px;
+    }
 `;
 
 export const Title = styled.h2`
@@ -15,13 +21,19 @@ export const Title = styled.h2`
     margin: 0;
     padding-bottom: 15px;
     letter-spacing: 0.05em;
-    border-bottom: 1px solid ${({ theme }) => theme.color.ironTransparent};
-    color: ${({ theme }) => theme.color.mineShaft};
+    border-bottom: 1px solid ${({ theme }) => theme.colors.buttonLink.ironTransparent};
+    color: ${({ theme }) => theme.colors.textPrimary};
+
+    @media(max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
+        font-size: clamp(18px, 6vw, 25px);
+        padding-bottom: 12px;
+        line-height: 22px;
+    }
 `;
 
 export const StyledList = styled.ul`
     font-weight: 400;
-    font-size: 18px;
+    font-size: clamp(14px, 5vw, 18px);
     line-height: 1.4;
     letter-spacing: 0.05em;
     list-style: none;
@@ -30,16 +42,30 @@ export const StyledList = styled.ul`
     display: grid;
     grid-template-columns: 1fr 1fr 1fr;
     grid-gap: 8px 95px;
+
+    @media(max-width: ${({ theme }) => theme.breakpoints.tabletHorizontalMax}px) {
+        grid-template-columns: 1fr 1fr;
+    }
+
+    @media(max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
+        grid-template-columns: 1fr;
+        padding: 12px 0 0 0;
+    }
 `;
 
 export const Item = styled.li`
     display: flex;
     align-items: center;
-    color: ${({ theme }) => theme.color.slateGray};
+    color: ${({ theme }) => theme.colors.site.text};
 `;
 
 export const Dot = styled(DotItem)`
     align-items: center;
     width: 9px;
     margin-right: 16px;
+
+    @media(max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
+        width: 8px;
+        margin-right: 8px;
+    }
 `;
