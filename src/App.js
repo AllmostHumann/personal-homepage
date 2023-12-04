eb6555d9fc1fe7b851c4248de8451fa84cbd23ec
Jakub Głuch <106307102+AllmostHumann@users.noreply.github.com>
2023-12-04 17:56:11 +0100

    normalize moved

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import { GlobalStyle } from "./features/Theme/GlobalStyle";
-import { Normalize } from "styled-normalize";
-import { ThemeProvider } from "styled-components";
-import { themeLight, themeDark } from "./features/Theme/theme";
-import { useSelector } from "react-redux";
-import { selectIsDarkTheme } from "./features/Theme/themeSlice";
-import PersonalHomepage from "./features/PersonalHomepage";
+import { GlobalStyle } from './features/Theme/GlobalStyle';
+import { ThemeProvider } from 'styled-components';
+import { themeLight, themeDark } from './features/Theme/theme';
+import { useSelector } from 'react-redux';
+import { selectIsDarkTheme } from './features/Theme/themeSlice';
+import PersonalHomepage from './features/PersonalHomepage';
 
 function App() {
   const isDarkTheme = useSelector(selectIsDarkTheme);
@@ -12,7 +11,6 @@ function App() {
   return (
     <ThemeProvider theme={isDarkTheme ? themeDark : themeLight}>
       <GlobalStyle />
-      <Normalize />
       <PersonalHomepage />
     </ThemeProvider>
   );
