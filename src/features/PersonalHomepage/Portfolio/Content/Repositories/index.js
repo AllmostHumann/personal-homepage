00252dd677a1809e8154c6ed282fe32424643f0c
Jakub Głuch <106307102+AllmostHumann@users.noreply.github.com>
2023-12-04 17:32:11 +0100

    key for list updated

--- src/features/PersonalHomepage/Portfolio/Content/Repositories/index.js
+++ src/features/PersonalHomepage/Portfolio/Content/Repositories/index.js
@@ -8,35 +8,45 @@ import {
   StyledLink,
   Tags,
   TagsContainer,
-} from "./styled";
+} from './styled';
 
 export const Repositories = ({ repositories }) => {
   return (
     <Wrapper>
-      {repositories.map(({ id, name, description, html_url, homepage, topics }) => (
-        <Container key={id}>
-          <Title>{name}</Title>
-          <Description>{description}</Description>
-          <LinkContainer>
-            <Text>Demo:</Text>
-            <StyledLink target="_blank" rel="noreferrer" href={homepage}>
-              Visit page
-            </StyledLink>
-          </LinkContainer>
-          <LinkContainer>
-            <Text>Code:</Text>
-            <StyledLink target="_blank" rel="noreferrer" href={html_url}>
-              Browse repository
-            </StyledLink>
-          </LinkContainer>
-          <TagsContainer>
-            <Text>Tags:</Text>
-              {topics.map((topic)=> (
-              <Tags>{topic}</Tags>
+      {repositories.map(
+        ({ id, name, description, html_url, homepage, topics }) => (
+          <Container key={id}>
+            <Title>{name}</Title>
+            <Description>{description}</Description>
+            <LinkContainer>
+              <Text>Demo:</Text>
+              <StyledLink
+                target='_blank'
+                rel='noreferrer'
+                href={homepage}
+              >
+                Visit page
+              </StyledLink>
+            </LinkContainer>
+            <LinkContainer>
+              <Text>Code:</Text>
+              <StyledLink
+                target='_blank'
+                rel='noreferrer'
+                href={html_url}
+              >
+                Browse repository
+              </StyledLink>
+            </LinkContainer>
+            <TagsContainer>
+              <Text>Tags:</Text>
+              {topics.map((topic) => (
+                <Tags>{topic}</Tags>
               ))}
-          </TagsContainer>
-        </Container>
-      ))}
+            </TagsContainer>
+          </Container>
+        ),
+      )}
     </Wrapper>
   );
 };
